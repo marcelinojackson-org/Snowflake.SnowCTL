f1a5fc3132757c4123b25e83ee91d70ae8cea919
Marcelino Jackson <marcelino.jackson@gmail.com>
2025-11-15 18:02:34 -0800

    chore: silence gosnowflake logs

--- cmd/snowctl/main.go
+++ cmd/snowctl/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"io"
 
-	"github.com/Snowflake-Labs/Snowflake.SnowCTL/pkg/cmd"
 	"github.com/sirupsen/logrus"
+	"github.com/snowflakedb/gosnowflake"
+
+	"github.com/Snowflake-Labs/Snowflake.SnowCTL/pkg/cmd"
 )
 
 func init() {
 	logrus.SetOutput(io.Discard)
+	logger := gosnowflake.GetLogger()
+	logger.SetOutput(io.Discard)
+	_ = logger.SetLogLevel("OFF")
 }
 
 func main() {
